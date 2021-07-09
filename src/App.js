cf410359081e53e185bccf946cc12afea08cc6ff
profnike <profnike@gmail.com>
2021-07-09 14:12:47 +0100

    Added Functional-Todo File

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Tasks from "./Components/Todo"
+import './Style/Todo.css'
+
+function App (props) {
+  
+  return (
+    <div className="App">
+     <Tasks/> 
+     <date/>
+    </div>
+  );
+
+}
+
+
+export default App;
